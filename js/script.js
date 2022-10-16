a0137233dd0577d58092d46022a66e07bc8d2556
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-10-16 14:05:27 +0530

    added the function to save a note

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ let noteList = [];
 
 let view = "grid"; //default view is grid-view
 
-function saveNote() {
-    
+function saveNote(title, content) {
+    var note = {
+        'title': title,
+        'content': content
+    }
+    var length = noteList.push(note);
+    console.log(length);
+    let node = document.createElement('p');
+    const successMessage = document.createTextNode("Note created Successfully!!");
+    node.appendChild(successMessage);
+    document.getElementsByClassName("accordion-body")[0].appendChild(node).style.color='green';
 }
 
 function deleteNote() {
